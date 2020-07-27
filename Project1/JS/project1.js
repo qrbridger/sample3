ea7b4519b1f67bc4f4373906352b53ec7051beab
qrbridger <68769157+qrbridger@users.noreply.github.com>
2020-07-27 15:30:52 +0530

    Update project1.js

--- Project1/JS/project1.js
+++ Project1/JS/project1.js
@@ -1,23 +1,9 @@
-function closeMe(){
-      // Find the element
-   		x=document.getElementById("demo");
-       //Option 1: Change the style attribute directly
-   		x.style.display="none";
-  
-      //Option 2: Change the class
-   		 // x.className="closed";
-	}
-
-function openMe(){
-      // Find the element
-   		x=document.getElementById("demo");
-      //Option 1: Change the style attribute directly
-   		 x.style.display="block";
-      
-      //Option 2: Change the class
-   		 // x.className="open";
-	}
 function sendEmail() 
 {
     window.location = "mailto:kaushal.singh007@gmail.com";
-}
+}   
+
+function Call() 
+{
+   window.open('tel:9986959691')
+}   
